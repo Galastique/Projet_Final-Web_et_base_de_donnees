c88e3dbba598de74f8a83e5e5872fcb4f0c050e8
Galastique <anthokess@hotmail.com>
2023-05-16 14:15:45 -0400

    ajoût éléments navigation

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -5,27 +5,37 @@ import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
-  const auth = useContext(AuthContext);
+    const auth = useContext(AuthContext);
 
-  return (
-    <ul className="nav-links">
-      <li>
-        <NavLink to="/" exact>
-          Accueil
-        </NavLink>
-      </li>
-      {auth.isLoggedIn && (
-        <li>
-          <NavLink to={`/${auth.userId}/connected`}>Connecté</NavLink>
-        </li>
-      )}
-      {!auth.isLoggedIn && (
-        <li>
-          <NavLink to={`/${auth.userId}/disconnected`}>Déconnecté</NavLink>
-        </li>
-      )}
-    </ul>
-  );
+    return (
+        <ul className="nav-links">
+            <li>
+                <NavLink to="/" exact>Accueil</NavLink>
+            </li>
+            <li>
+                <NavLink to="/" exact>Déroulement Stage Employeurs</NavLink>
+            </li>
+            <li>
+                <NavLink to="/" exact>Profils étudiants</NavLink>
+            </li>
+            <li>
+                <NavLink to="/" exact>Déroulement Stage Étudiants</NavLink>
+            </li>
+            <li>
+                <NavLink to="/" exact>FAQ</NavLink>
+            </li>
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to={`/${auth.userId}/connected`}>Connecté</NavLink>
+                </li>
+            )}
+            {!auth.isLoggedIn && (
+                <li>
+                    <NavLink to={`/${auth.userId}/disconnected`}>Déconnecté</NavLink>
+                </li>
+            )}
+        </ul>
+    );
 };
 
 export default NavLinks;
