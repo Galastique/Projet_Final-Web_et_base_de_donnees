27666c172dca4ff7ad71c7277b9ed675b2355ff1
Lorenzo Mignacca <2213015@cmontmorency.qc.ca>
2023-05-18 01:05:29 -0400

    Page informatives statiques (non terminées)
    (Seulement accueil & déroulement étudiants faits)
    + Mise en forme du CSS des pages (pas en-tête ou pied de page)

--- frontend/src/accueil/pages/Accueil.js
+++ frontend/src/accueil/pages/Accueil.js
@@ -1,37 +1,38 @@
 import React from "react";
 
 const Accueil = (props) => {
-    return <div>
+  return (
+    <div className="main_content">
+      <h2 className="main_content-title">Édition 2023</h2>
+      <h4 className="main_content-subtitle">
+        Bienvenue sur le site des stages de fin d'études des techniques de
+        l'informatique du Collège Montmorency!
+      </h4>
+      <hr />
+      <div className="main_content-content">
         <p>
-            Édition 2023<br/><br/>
-
-            Bienvenue sur le site des stages de fin d'études des
-            techniques de l'informatique du Collège Montmorency!<br/><br/>
-
-            À la fin de leurs études,
-            les étudiants sont appelés à mettre en
-            pratique les compétences acquises durant le programme.
-            Cela se fait grâce à la participation d'entreprises de la
-            région qui les accueillent afin de finaliser leurs
-            formations.<br/><br/>
-
-            Le Collège Montmorency
-            offre ainsi aux employeurs l'occasion d'obtenir une
-            main-d'œuvre compétente, tout en leur permettant de
-            participer à la formation finale des
-            étudiants.<br/><br/>
-
-            Le stage de fin d'études
-            est une expérience concrète permettant d'acquérir une
-            expérience professionnelle formatrice.<br/><br/>
-
-            Les étudiants terminent
-            la portion académique de leurs études en informatique
-            selon une des deux voies de sortie du programme:
-            Réseaux et sécurité informatique
-            Développement d'applications informatiques<br/><br/>
+          À la fin de leurs études, les étudiants sont appelés à mettre en
+          pratique les compétences acquises durant le programme. Cela se fait
+          grâce à la participation d'entreprises de la région qui les
+          accueillent afin de finaliser leurs formations.
         </p>
-    </div>;
+        <p>
+          Le Collège Montmorency offre ainsi aux employeurs l'occasion d'obtenir
+          une main-d'œuvre compétente, tout en leur permettant de participer à
+          la formation finale des étudiants.
+        </p>
+        <p>
+          Le stage de fin d'études est une expérience concrète permettant
+          d'acquérir une expérience professionnelle formatrice.
+        </p>
+        <p>
+          Les étudiants terminent la portion académique de leurs études en
+          informatique selon une des deux voies de sortie du programme: Réseaux
+          et sécurité informatique Développement d'applications informatiques
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Accueil;
