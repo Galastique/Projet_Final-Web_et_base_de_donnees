4a38ed855f3c987521b557b0350366dbcae4f58e
Lorenzo Mignacca <2213015@cmontmorency.qc.ca>
2023-05-17 23:45:14 -0400

    Création du footer

--- frontend/src/shared/components/Footer.js
+++ frontend/src/shared/components/Footer.js
@@ -3,7 +3,15 @@ import React from "react";
 import "./Footer.css";
 
 const MainNavigation = (props) => {
-  return <footer className="main_footer">penis</footer>;
+  return (
+    <footer className="main_footer">
+      <p>
+        Coordonnateur des stages: Sylvain Labranche
+        (sylvain.labranche@cmontmorency.qc.ca)
+      </p>
+      <p>Dernière modification: [Insérer la date ici]</p>
+    </footer>
+  );
 };
 
 export default MainNavigation;
