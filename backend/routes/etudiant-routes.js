225d024b5ec80170d25f9f561fefcf329118a74b
Galastique <anthokess@hotmail.com>
2023-05-19 18:53:30 -0400

    ajoût routes oubliées

--- backend/routes/etudiant-routes.js
+++ backend/routes/etudiant-routes.js
@@ -5,6 +5,8 @@ const router = express.Router();
 router.get("/", controllerEtudiant.getEtudiants);
 router.post("/", controllerEtudiant.ajouterEtudiant);
 router.get("/:etudiantId", controllerEtudiant.accederEtudiant);
+router.patch("/:etudiantId/modifier", controllerEtudiant.modifierEtudiant);
 router.patch("/:etudiantId/inscrire", controllerEtudiant.inscrireEtudiant);
+router.delete("/:etudiantId", controllerEtudiant.supprimerEtudiant);
 
 module.exports = router;
