cd34ca83106270c9f647304795f24bb61a29b0db
Galastique <anthokess@hotmail.com>
2023-05-19 17:19:53 -0400

    ajoût et modifications routes

--- backend/routes/stage.js
+++ backend/routes/stage.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.get("/", controllerStage.getStages);
 router.post("/", controllerStage.ajouterStage);
 router.get("/:stageId", controllerStage.accederStage);
+router.patch("/:stageId/modifier", controllerStage.modifierStage);
+router.delete("/:stageId", controllerStage.supprimerStage);
 
 module.exports = router;
